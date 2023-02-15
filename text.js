c00f19a30cc7f0ebde4b12f25bf2955c111e9476
yuki-koike <95168477+yuki-koike@users.noreply.github.com>
2023-02-15 16:21:36 +0900

    Add files via upload

---
+++ text.js
@@ -0,0 +1,23 @@
+var textJsStores = {
+	//'店舗名' : <B>店舗名<BR>TEL.000-0000<BR>Email:sample@aaa.com<BR>までお問い合わせください。</BR>
+	'aa店' : '<B>株式会社XXaa店</B><BR>TEL.000-0000<BR>Email:sample@aaa.com<BR>までお問い合わせください。<BR>',
+	'bb店' :'<B>株式会社XXbb店</B><BR>TEL.000-0000<BR>Email:sample@aaa.com<BR>までお問い合わせください。<BR>',
+	'cc店' : '<B>株式会社XXcc店</B><BR>TEL.000-0000<BR>Email:sample@aaa.com<BR>までお問い合わせください。<BR>',
+	'dd店' : '<B>株式会社XXdd店</B><BR>TEL.000-0000<BR>Email:sample@aaa.com<BR>までお問い合わせください。<BR>'
+}
+
+var postageImage = {
+	'B4' : 'souryo_11',
+	'B1' : 'souryo_1',
+	'B2' : 'souryo_2x',
+	'B3' : 'souryo_4',
+	'B5' : 'yamato40',
+	'B12' : 'yamato60',
+	'B6' : 'yamato80',
+	'B13' : 'yamato100',
+	'B7' : 'yamato120',
+	'B14' : 'yamato140',
+	'B8' : 'yamato160',
+	'B9' : 'yamato180',
+	'B10' : 'yamato200'
+}
